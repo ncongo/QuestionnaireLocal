cca33bfa05f4e5a9dda7a852ac7d6f52229beec6
ncongo <ncongo1@etf.unsa.ba>
2018-02-26 00:15:33 +0100

    deleted

--- QuestionnaireApi/dbconnection.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var connection = mysql.createPool({
-	host: 'localhost',
-	user: 'mop',
-	password: 'mopDB123!',
-	database: 'mopquestionnaire',
-	multipleStatements: true
-});
-module.exports = connection;
